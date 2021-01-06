bdf11260a1e26f0c2aab00547c99e9e99e87bc35
nagellette <necipenesgengec@gmail.com>
2021-01-06 21:21:49 +0300

    Added dice loss and miou metric. Added analysis folder and jupyter notebook for output analysis. Converted input file jsons into new input data paths and filenames.

--- utils/custom_losses.py
+++ utils/custom_losses.py
@@ -4,8 +4,15 @@ import tensorflow as tf
 from tensorflow.keras.losses import Loss
 import tensorflow.keras.backend as K
 
+"""
+Dice loss is derived from: https://www.kaggle.com/bigironsphere/loss-function-library-keras-pytorch
+"""
+
 
 def dice_loss(y_true, y_pred, smooth=1):
+    """
+    Dice loss function.
+    """
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
     intersection = K.sum(y_true_f * y_pred_f)
