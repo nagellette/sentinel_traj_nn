f86024851a553454674c529a74bb7f1d298edd62
ngengec <necipenesgengec@gmail.com>
2022-06-18 12:07:57 +0300

    Hot fix get_fusion_layer.py

--- utils/get_fusion_layer.py
+++ utils/get_fusion_layer.py
@@ -11,13 +11,13 @@ def get_fusion_layer(fusion_type, input1, input2):
 
     """
     if fusion_type == "average":
-        return tf.Keras.layers.Average()([input1, input2])
+        return tf.keras.layers.Average()([input1, input2])
     elif fusion_type == "sum":
-        return tf.Keras.layers.Add()([input1, input2])
+        return tf.keras.layers.Add()([input1, input2])
     elif fusion_type == "multiply":
-        return tf.Keras.layers.Multiply()([input1, input2])
+        return tf.keras.layers.Multiply()([input1, input2])
     elif fusion_type == "concat":
-        return tf.Keras.layers.Concatenate(axis=1)([input1, input2])
+        return tf.keras.layers.Concatenate(axis=1)([input1, input2])
     else:
         print("Fusion type not defined!")
         return -1
